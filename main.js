d665976c8161b59c489b9ca8f585f2c8956dab68
Omar-O-5d <137425794+Omar-O-5d@users.noreply.github.com>
2024-02-13 22:33:34 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+img="";
+status="";
+objects=[];
+
+function preload() {
+    img=loadImage('dog_cat.jpg');
+    
+}
+
+function setup() {
+    canvas=createCanvas(380,380);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.size(380,380);
+    video.hide();
+}
+
+function start() {
+    objectDetector=ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML="Status : Detecting objects";
+}
+
+
+function draw() {
+    image(video,0,0,380,380);
+
+    if (status != "") {
+        r=random(255);
+        g=random(255);
+        b=random(255);
+objectDetector.detect(video,gotResult);
+        for (i= 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML="Status : Baby detected";
+
+            fill(r,g,b);
+            percent=floor(objects[i].confidence *100);
+        text(objects[i].label +""+ percent + "%", objects[i].x + 15, objects[i].y +15);
+        noFill();
+        stroke(r,g,b);
+        rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+        if (objects.length< 1) {
+            document.getElementById("status").innerHTML="Status : Baby NOT :( detected";
+            play();
+          } else{
+                document.getElementById("status").innerHTML="Status : Baby detected";
+                stop();
+          }
+        }
+       
+
+    
+
+   
+}
+
+function modelLoaded() {
+    console.log("model loaded");
+    status=true;
+}
+
+function gotResult(error,results) {
+    if (error) {
+        console.log(error);   
+    }
+    else {
+        console.log(results);  
+        objects=results;  
+    }
+}
+
+function play() {
+    play_sound()=a.mp3.play();
+}
+
+function play() {
+    play_sound()=a.mp3.stop();
+}
